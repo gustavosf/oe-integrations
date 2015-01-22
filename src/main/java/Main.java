6a0df51fb89faebe2319a7f6080896b99a45739d
Gustavo Seganfredo <gustavo.farias@objectedge.com>
2015-01-22 11:41:32 -0200

    Trabalhando no processador de eventos de pull request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,75 +1,62 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main extends HttpServlet {
+
+  //*************************************************************************//
+  //***  OVERRIDES   ********************************************************//
+  //*************************************************************************//
+
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
+    // Processa o header para verificar qual o evento que causou o hook
     Map<String, String> headers = getHeadersInfo(req);
+    String hook = headers.get("X-Github-Event");
+    String message;
 
-    String hook = headers.get("X-GitHub-Event");
-
-    if (hook == "pull_request") processPullRequest(getJSONPayload(req));
-
-  }
-
-  protected void processPullRequest(JSONObject payload) {
-
-    // int someInt = payload.getInt("intParamName");
-    // String someString = payload.getString("stringParamName");
-    // JSONObject nestedObj = payload.getJSONObject("nestedObjName");
-    // JSONArray arr = payload.getJSONArray("arrayParamName");
-
-    // String responseMessage;
-    // responseMessage = ""
-
-  }
-
-  /**
-   * Retorna o paylaod enviado pelo webhook
-   * @return Payload
-   */
-  private JSONObject getJSONPayload(HttpServletRequest req) {
-    StringBuffer jb = new StringBuffer();
-    String line = null;
-
-    try {
-      BufferedReader reader = req.getReader();
-      while ((line = reader.readLine()) != null)
-        jb.append(line);
-    } catch (Exception e) { /*report an error*/ }
-
-    try {
-      JSONObject jsonPayload = JSONObject.fromObject(jb.toString());
-    } catch (ParseException e) {
-      // crash and burn
-      throw new IOException("Error parsing JSON request string");
+    // Roteia para o método certo gerar a mensagem
+    if (hook == null) {
+      message = "Did not receive an event hook";
+    } else if (hook.equals("pull_request")) {
+      message = processPullRequest(getJSONPayload(req));
+    } else {
+      message = "Event hook \"" + hook + "\" not implemented yet";
     }
 
-    return jsonPayload;
-  }
+    // Gera uma instância do bot
+    SlackBot bot = new SlackBot(System.getenv("SLACK_HOST"),
+            System.getenv("SLACK_TOKEN"));
 
-  /**
-   * Retorna os headers da requisição
-   * @return Headers
-   */
-  private Map<String, String> getHeadersInfo(HttpServletRequest req) {
-    Map<String, String> map = new HashMap<String, String>();
-    Enumeration headerNames = request.getHeaderNames();
-    while (headerNames.hasMoreElements()) {
-      String key = (String) headerNames.nextElement();
-      String value = request.getHeader(key);
-      map.put(key, value);
-    }
+    // Manda a mensagem
+    bot.message("#tests", message);
 
-    return map;
+    // Mostra um feedback na tela, só para debug caso necessário
+    PrintWriter writer = resp.getWriter();
+    writer.print(message);
+
+
+//    for (Map.Entry<String, String> entry : headers.entrySet())
+//    {
+//      writer.print(entry.getKey() + "/" + entry.getValue());
+//    }
+
   }
 
   @Override
@@ -89,4 +76,88 @@ public class Main extends HttpServlet {
     server.start();
     server.join();
   }
+
+  //*************************************************************************//
+  //***  HOOK PROCESSORS   **************************************************//
+  //*************************************************************************//
+
+  /**
+   * Processa um evento de Pull Request
+   * @param  payload Payload enviado pelo github
+   * @return         Mensagem que o bot deverá enviar ao canal
+   */
+  protected String processPullRequest(JSONObject payload) {
+
+    JSONObject pullRequest = payload.optJSONObject("pull_request");
+    JSONObject user = pullRequest.optJSONObject("user");
+    JSONObject repo = payload.optJSONObject("repository");
+    JSONObject head = pullRequest.optJSONObject("head");
+    JSONObject base = pullRequest.optJSONObject("base");
+    String message;
+
+    // Monta a mensagem a ser enviada para o slack
+    message = ":octocat: New pull request for the repo *";
+    message = message + repo.optString("name");
+    message = message + "*:\n*";
+    message = message + user.optString("login");
+    message = message + "* wants to merge *";
+    message = message + pullRequest.optString("number");
+    message = message + " commit(s)* into *";
+    message = message + base.optString("label");
+    message = message + "* from *";
+    message = message + head.optString("label");
+    message = message + "\n";
+    message = message + pullRequest.optString("title");
+    message = message + "\n";
+    message = message + pullRequest.optString("html_url");
+
+    return message;
+
+  }
+
+  //*************************************************************************//
+  //***  FUNÇÕES AUXILIARES   ***********************************************//
+  //*************************************************************************//
+
+  /**
+   * Retorna o paylaod enviado pelo webhook
+   * @return Payload
+   */
+  private JSONObject getJSONPayload(HttpServletRequest req) {
+    StringBuffer buffer = new StringBuffer();
+    String line = null;
+
+    try {
+      BufferedReader reader = req.getReader();
+      while ((line = reader.readLine()) != null)
+        buffer.append(line);
+    } catch (Exception e) { /*report an error*/ }
+
+    JSONObject jsonPayload = null;
+    try {
+      jsonPayload = new JSONObject(buffer.toString());
+    } catch (JSONException e) {
+      jsonPayload = null;
+    }
+
+    return jsonPayload;
+
+  }
+
+  /**
+   * Retorna os headers da requisição
+   * @return Headers
+   */
+  private Map<String, String> getHeadersInfo(HttpServletRequest req) {
+    Map<String, String> map = new HashMap<String, String>();
+    Enumeration headerNames = req.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String key = (String) headerNames.nextElement();
+      String value = req.getHeader(key);
+      map.put(key, value);
+    }
+
+    return map;
+  }
+
 }
---
+++ src/main/java/SlackBot.java
@@ -0,0 +1,54 @@
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/**
+ * Abstração para comandos do SlackBot
+ *
+ * Created by gustavo on 22/01/15.
+ */
+public class SlackBot {
+
+    private final String host;
+    private final String token;
+
+    String postResource= "https://%s/services/hooks/slackbot";
+
+    /**
+     * Constructor
+     * @param host  Host da empresa no slack
+     * @param token Token gerado pelo slack para integração com o bot
+     */
+    public SlackBot(String host, String token) {
+        this.host = host;
+        this.token = token;
+    }
+
+    //*************************************************************************//
+    //***  COMANDOS   *********************************************************//
+    //*************************************************************************//
+
+    /**
+     * Envia uma mensagem para um determinado canal
+     * @param channel Canal em que o bot deverá enviar a mensagem
+     * @param message Mensagem a ser enviada
+     * @return        true se a requisição foi enviada com sucesso
+     */
+    public Boolean message(String channel, String message) {
+
+        String url = String.format(postResource, this.host);
+
+        try {
+            Unirest.post(url)
+                   .queryString("token", this.token)
+                   .queryString("channel", channel)
+                   .body(message)
+                   .asBinary();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+}
